613f5c197734eeaf29c316ff666371ae57388985
LitFlowXue <50521429+LitFlowXue@users.noreply.github.com>
2021-03-14 17:09:27 +0800

    Add files via upload

---
+++ mario more.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = get_int("Height: ");
+    while (n<1 || n>8)
+    { 
+        n = get_int("Height: ");
+    }
+
+
+   for (int i=0; i<n; i++)
+    {
+        for (int b=n-1; b>i; b--)
+        {
+            printf(" ");
+        }
+
+        for (int a=0; a<i+1; a++) 
+        {
+            printf("#");
+        }
+
+    printf("  ");
+
+     for (int b=0; b<i+1; b++) 
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+}
